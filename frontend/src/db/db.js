de71237031af2247967cc9bf32e4fe8927fbb7d7
Pierre Le Fevre <hello@pierrelf.com>
2023-09-28 18:24:57 +0200

    Fix imports frontend

--- frontend/src/db/db.js
+++ frontend/src/db/db.js
@@ -1,29 +1,29 @@
-import mongodb from 'mongodb';
+import mongodb from "mongodb";
 
 export const getClient = () => {
-    const client = mongodb.MongoClient(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    return client;
-}
+  const client = mongodb.MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  return client;
+};
 
 export const getDb = () => {
-    const client = getClient();
-    return client.db("at");
-}
+  const client = getClient();
+  return client.db("at");
+};
 
 export const getCollection = (collection) => {
-    const db = getDb();
-    return db.collection(collection);
-}
+  const db = getDb();
+  return db.collection(collection);
+};
 
-export const getStories = async () => {
-    const stories = await getCollection("stories").find({}).toArray();
-    return stories;
-}
+export const getStories = () => {
+  const stories = getCollection("stories").find({}).toArray();
+  return stories;
+};
 
-export const getGroups = async () => {
-    const groups = await getCollection("groups").find({}).toArray();
-    return groups;
-}
+export const getGroups = () => {
+  const groups = getCollection("groups").find({}).toArray();
+  return groups;
+};
