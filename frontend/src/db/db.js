de08809566e5a78922cf4961d86f18c553040d2d
Pierre Le Fevre <hello@pierrelf.com>
2023-10-02 13:00:39 +0200

    Reduce number of stories loaded, fixes #8

--- frontend/src/db/db.js
+++ frontend/src/db/db.js
@@ -20,11 +20,43 @@ export const getCollection = (collection) => {
 };
 
 export const getStories = async () => {
-  const stories = await getCollection("stories").find({}).sort({ published_at: -1 }).toArray();
+  const stories = await getCollection("stories")
+    .find({})
+    .sort({ published_at: -1 })
+    .toArray();
   return stories;
 };
 
 export const getGroups = async () => {
   const groups = await getCollection("groups").find({}).toArray();
   return groups;
+};
+
+export const getLatestStory = async () => {
+  return getCollection("stories")
+    .find({})
+    .sort({ published_at: -1 })
+    .limit(1)
+    .toArray();
+};
+
+export const getStoriesById = async (ids) => {
+  const stories = await getCollection("stories")
+    .find({ id: { $in: ids } })
+    .toArray();
+  return stories;
+};
+
+export const getStoriesByTopic = async (topic) => {
+  // find all stories with the given topic, not case sensitive.
+  const stories = await getCollection("stories")
+    .find({ category: { $regex: topic, $options: "i" } })
+    .sort({ published_at: -1 })
+    .toArray();
+  return stories;
+};
+
+export const getStoryById = async (id) => {
+  const story = await getCollection("stories").findOne({ id: id });
+  return story;
 };
